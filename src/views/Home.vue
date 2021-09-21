fd8dd317efb63fbf3c8a2fb228c0a45ce5965519
ajitimur <ajimtimur@gmail.com>
2021-09-21 17:44:13 +0700

    set up

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+	<div class="row">
+    <home-card></home-card>
+  </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import HomeCard from "@/components/HomeCard.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HomeCard,
+  },
+};
+</script>
